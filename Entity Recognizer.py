42e94847f1c6a613638950828f6e5b20d3f9ff9a
Shashank Bhatt <bshank007@gmail.com>
2017-06-06 08:17:49 +0530

    Update Entity Recognizer.py

--- Entity Recognizer.py
+++ Entity Recognizer.py
@@ -1,6 +1,7 @@
 import pycrfsuite
 import shutil,io
 import spacy
+from data import getData
 #import Span
 nlp = spacy.load('en')
 
@@ -29,3 +30,61 @@ def _from_json_to_crf(json_eg, spacy_nlp):
 
         crf_format = [(doc[i].text, doc[i].tag_, ent_clean(ents[i])) for i in range(len(doc))]
         return crf_format
+
+train_data=getData()
+##convert the train data into crf format i.e tagging of each word in each document of train data
+dataset = [_from_json_to_crf(q, nlp) for q in train_data]
+
+print (sent2features(dataset[0])[0])
+
+
+#convert text to features and labels i.e entitys 
+x = [sent2features(s) for s in dataset]
+y = [sent2labels(s) for s in dataset]
+
+
+trainer = pycrfsuite.Trainer(verbose=False)
+
+for X, Y in zip(x, y):
+    trainer.append(X,Y)
+
+
+trainer.set_params({
+    'c1': 1.0,   # coefficient for L1 penalty
+    'c2': 1e-3,  # coefficient for L2 penalty
+    'max_iterations': 50,  # stop earlier
+
+    # include transitions that are possible, but not observed
+    'feature.possible_transitions': True
+})
+
+print (trainer.params())
+
+trainer.train('entity_model.crfsuite')
+print (trainer.logparser.last_iteration)
+
+
+# Make Predictions
+
+tagger = pycrfsuite.Tagger()
+tagger.open('entity_model.crfsuite')
+
+
+doc = nlp('update email of google account'.decode('utf-8'))##id,Id
+crf_format = [(doc[i].text, doc[i].tag_,'N/A') for i in range(len(doc))]
+print(crf_format)
+
+
+print("Predicted:", ' '.join(tagger.tag(sent2features(crf_format))))
+
+
+# print the word and entity associated with it
+ents=tagger.tag(sent2features(crf_format))
+for i in range(0,len(crf_format)):
+    if ents[i]!='O':
+        print (crf_format[i][0])
+        print (ents[i])
+
+
+
+
